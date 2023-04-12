da1d40eb3b4c0c614035b06d8f87084e0e05c4e6
tanhun <tanhoang0710@gmail.com>
2023-04-12 13:09:16 +0700

    user profile

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -4,6 +4,7 @@ const {
   loginController,
   testController,
   forgotPasswordController,
+  updateProfileController,
 } = require("../controllers/authController");
 const { requireSignIn, isAdmin } = require("../middlewares/authMiddleware");
 
@@ -32,4 +33,8 @@ router.get("/user-auth", requireSignIn, (req, res) => {
 router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
   res.status(200).send({ ok: true });
 });
+
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
+
 module.exports = router;
